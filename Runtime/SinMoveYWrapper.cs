09fa812a9b9881d282b77df2a1b6f75ec1c9bfd9
Timothy Edrick Burhan <timothy@agate.id>
2023-11-24 12:36:18 +0700

    feat: added sine move Y Wrapper

---
+++ Runtime/SinMoveYWrapper.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace StinkySteak.NetcodeBenchmark
+{
+    [System.Serializable]
+    public struct SinMoveYWrapper
+    {
+        [SerializeField] private float _minSpeed;
+        [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _minAmplitude;
+        [SerializeField] private float _maxAmplitude;
+        [SerializeField] private float _randomMultiplier;
+
+        private Vector3 _initialPosition;
+
+        private float _speed;
+        private float _amplitude;
+
+        public static SinMoveYWrapper CreateDefault()
+        {
+            SinMoveYWrapper wrapper = new();
+            wrapper._minSpeed = 0.5f;
+            wrapper._maxSpeed = 1f;
+            wrapper._minAmplitude = 0.5f;
+            wrapper._maxAmplitude = 1f;
+            wrapper._randomMultiplier = 5f;
+
+            return wrapper;
+        }
+
+        public void NetworkStart()
+        {
+            _speed = Random.Range(_minSpeed, _maxSpeed);
+            _amplitude = Random.Range(_minAmplitude, _maxAmplitude);
+            _initialPosition = GenerateRandomPosition();
+        }
+
+        private Vector3 GenerateRandomPosition()
+        {
+            return new Vector3(
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f),
+                Random.Range(-1f, 1f)
+                ) * _randomMultiplier;
+        }
+
+        public void NetworkUpdate(Transform transform)
+        {
+            float sin = Mathf.Sin(Time.time * _speed) * _amplitude;
+
+            transform.position = _initialPosition + (Vector3.up * sin);
+        }
+    }
+}
